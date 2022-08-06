250542ceb66be871b45559cbac8bad96afa7df02
The Merry Rogue <108585847+TheMerryRogue@users.noreply.github.com>
2022-08-05 17:50:21 -0700

    Initial upload

---
+++ main/java/net/merryrogue/rhdyeanotherday/DyeAnotherDayClient.java
@@ -0,0 +1,10 @@
+package net.merryrogue.rhdyeanotherday;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DyeAnotherDayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
